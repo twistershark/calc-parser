4e53c039646cf89ce535a22352e7ed310a3b9717
Paulo Victor <paulovmel@gmail.com>
2021-05-21 23:14:30 -0300

    feat: updates aluno.py info

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "Paulo Victor da Silva"
+matricula = "18/0129058"
+email = "paulovmel@gmail.com"
+usuario_github = "twistershark"
