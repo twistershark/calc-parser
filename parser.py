2ce1ab7f61d977c215d331a9cfbe9416440bd987
Paulo Victor <paulovmel@gmail.com>
2021-05-21 23:17:53 -0300

    feat: implements grammar for Calc Parser

--- parser.py
+++ parser.py
@@ -2,17 +2,36 @@ import re
 import math
 from lark import Lark, InlineTransformer, Token
 
-
-# Implemente a gramática aqui! Você pode testar manualmente seu código executando
-# o arquivo calc.py e testá-lo utilizando o pytest.
 grammar = Lark(
     r"""
-start : /\d+/
-""",
-    parser="lalr",
+    ?start  : assign* comp?
+    ?assign: NAME "=" comp
+    ?comp  : expr "<" expr  -> lt
+        | expr "<=" expr -> le
+        | expr ">" expr  -> gt
+        | expr ">=" expr -> ge
+        | expr "!=" expr -> ne
+        | expr "==" expr -> eq
+        | expr
+    ?term  : term "*" pow   -> mul
+        | term "/" pow   -> div
+        | pow
+    ?expr  : expr "-" term  -> sub
+        | expr "+" term  -> add
+        | term
+    ?pow   : atom "^" pow   -> exp
+        | atom
+    ?atom  : NUMBER                        -> number
+        | NAME "(" expr ")"             -> func
+        | NAME "(" expr ("," expr)* ")" -> func
+        | NAME                          -> var
+        | "(" expr ")"
+    NAME   : /[-+]?[A-Za-z0-9_]+/
+    NUMBER : /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/
+    %ignore /\s+/
+    %ignore /\#.*/
+    """
 )
-
-
 class CalcTransformer(InlineTransformer):
     from operator import add, sub, mul, truediv as div, pow as exp, gt, ge, lt, le, ne, eq
 
@@ -50,7 +69,7 @@ class CalcTransformer(InlineTransformer):
                 return self.vars[token]
         except KeyError:
             return "Invalid: " + str(token)
-
+    
     def start(self, *args):
         return args[-1]
 
